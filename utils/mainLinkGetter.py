b0ae1e4a5f1d127a8494fc5ebce75089913edb10
Pat-Laugh <p.laughrea@gmail.com>
2017-03-06 18:37:55 -0500

    Addition of utils to get file names
    The utils are to be called by redirecting the output of the list of
    files to their input.

---
+++ utils/mainLinkGetter.py
@@ -0,0 +1,9 @@
+import sys, re
+
+p = re.compile(r'([^.]*)\.gitignore')
+
+for line in sys.stdin:
+    m = p.match(line)
+    if (m):
+        s = m.group(1)
+        print("'%s' : '%s'" % (s.lower(), s))
