989cb0fe5563f7669d19001ae2306cc45965553b
Pat-Laugh <p.laughrea@gmail.com>
2017-03-15 23:09:32 -0400

    Removal of unnecessary files

--- utils/mainLinkGetter.py
+++
@@ -1,9 +0,0 @@
-import sys, re
-
-p = re.compile(r'([^.]*)\.gitignore')
-
-for line in sys.stdin:
-    m = p.match(line)
-    if (m):
-        s = m.group(1)
-        print("'%s' : '%s'," % (s.lower(), s))
